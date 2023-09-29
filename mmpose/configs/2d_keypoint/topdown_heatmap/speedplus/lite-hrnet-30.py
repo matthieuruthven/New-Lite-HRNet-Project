4fdb59559cc5c9ac286ad83f43027cebe3ffd7f9
Matthieu Ruthven <matthieu.ruthven@uni.lu>
2023-09-29 17:25:57 +0200

    Config files

--- mmpose/configs/2d_keypoint/topdown_heatmap/speedplus/lite-hrnet-30.py
+++ mmpose/configs/2d_keypoint/topdown_heatmap/speedplus/lite-hrnet-30.py
@@ -10,12 +10,11 @@ from mmengine.optim import LinearLR, MultiStepLR
 from torch.optim import Adam
 
 from mmpose.codecs import UDPHeatmap
-from mmpose.datasets import (CocoDataset, GenerateTarget, GetBBoxCenterScale,
-                             LoadImage, PackPoseInputs, RandomFlip,
-                             RandomHalfBody, TopdownAffine)
+from mmpose.datasets import (SpeedPlusDataset, GenerateTarget,
+                             LoadImage, PackPoseInputs)
 from mmpose.datasets.transforms.common_transforms import RandomBBoxTransform
-from mmpose.evaluation import CocoMetric
-from mmpose.models import (HeatmapHead, HRNet, KeypointMSELoss,
+from mmpose.evaluation import PCKAccuracy
+from mmpose.models import (HeatmapHead, LiteHRNet, KeypointMSELoss,
                            PoseDataPreprocessor, TopdownPoseEstimator)
 
 # runtime
@@ -24,7 +23,7 @@ train_cfg.update(max_epochs=210, val_interval=10)
 # optimizer
 optim_wrapper = dict(optimizer=dict(
     type=Adam,
-    lr=5e-4,
+    lr=2e-3,
 ))
 
 # learning policy
@@ -34,21 +33,21 @@ param_scheduler = [
     dict(
         type=MultiStepLR,
         begin=0,
-        end=210,
-        milestones=[170, 200],
+        end=100,
+        milestones=[45, 60],
         gamma=0.1,
         by_epoch=True)
 ]
 
 # automatically scaling LR based on the actual training batch size
-auto_scale_lr = dict(base_batch_size=512)
+auto_scale_lr = dict(base_batch_size=4)
 
 # hooks
-default_hooks.update(checkpoint=dict(save_best='coco/AP', rule='greater'))
+default_hooks.update(checkpoint=dict(save_best='speedplus/PCK', rule='greater'))
 
 # codec settings
 codec = dict(
-    type=UDPHeatmap, input_size=(192, 256), heatmap_size=(48, 64), sigma=2)
+    type=UDPHeatmap, input_size=(640, 640), heatmap_size=(160, 160), sigma=2)
 
 # model settings
 model = dict(
@@ -59,93 +58,77 @@ model = dict(
         std=[58.395, 57.12, 57.375],
         bgr_to_rgb=True),
     backbone=dict(
-        type=HRNet,
+        type=LiteHRNet,
         in_channels=3,
         extra=dict(
-            stage1=dict(
-                num_modules=1,
-                num_branches=1,
-                block='BOTTLENECK',
-                num_blocks=(4, ),
-                num_channels=(64, )),
-            stage2=dict(
-                num_modules=1,
-                num_branches=2,
-                block='BASIC',
-                num_blocks=(4, 4),
-                num_channels=(48, 96)),
-            stage3=dict(
-                num_modules=4,
-                num_branches=3,
-                block='BASIC',
-                num_blocks=(4, 4, 4),
-                num_channels=(48, 96, 192)),
-            stage4=dict(
-                num_modules=3,
-                num_branches=4,
-                block='BASIC',
-                num_blocks=(4, 4, 4, 4),
-                num_channels=(48, 96, 192, 384))),
+            stem=dict(  
+                stem_channels=32,
+                out_channels=32,
+                expand_ratio=1),
+            num_stages=3,
+            stages_spec=dict(
+                num_modules=(3, 8, 3),
+                num_branches=(2, 3, 4),
+                num_blocks=(2, 2, 2),
+                module_type=('LITE', 'LITE', 'LITE'),
+                with_fuse=(True, True, True),
+                reduce_ratios=(8, 8, 8),
+                num_channels=(
+                    (40, 80),
+                    (40, 80, 160),
+                    (40, 80, 160, 320)))),
         init_cfg=dict(
             type=PretrainedInit,
-            checkpoint='https://download.openmmlab.com/mmpose/'
-            'pretrain_models/hrnet_w48-8ef0771d.pth'),
+            checkpoint='https://download.openmmlab.com/mmpose/top_down/litehrnet/litehrnet30_mpii_256x256-faae8bd8_20210622.pth')
     ),
     head=dict(
         type=HeatmapHead,
-        in_channels=48,
-        out_channels=17,
+        in_channels=40,
+        out_channels=11,
         deconv_out_channels=None,
         loss=dict(type=KeypointMSELoss, use_target_weight=True),
         decoder=codec),
     test_cfg=dict(
-        flip_test=True,
+        flip_test=False,
         flip_mode='heatmap',
         shift_heatmap=False,
     ))
 
 # base dataset settings
-dataset_type = CocoDataset
+dataset_type = SpeedPlusDataset
 data_mode = 'topdown'
-data_root = 'data/coco/'
+data_root = '../space_cluster/speedplus_cropped'
 
 backend_args = dict(backend='local')
 
 # pipelines
 train_pipeline = [
     dict(type=LoadImage, backend_args=backend_args),
-    dict(type=GetBBoxCenterScale),
-    dict(type=RandomFlip, direction='horizontal'),
-    dict(type=RandomHalfBody),
-    dict(type=RandomBBoxTransform),
-    dict(type=TopdownAffine, input_size=codec['input_size'], use_udp=True),
     dict(type=GenerateTarget, encoder=codec),
     dict(type=PackPoseInputs)
 ]
 val_pipeline = [
     dict(type=LoadImage, backend_args=backend_args),
-    dict(type=GetBBoxCenterScale),
-    dict(type=TopdownAffine, input_size=codec['input_size'], use_udp=True),
     dict(type=PackPoseInputs)
 ]
 
 # data loaders
 train_dataloader = dict(
-    batch_size=32,
-    num_workers=2,
+    batch_size=4,
+    num_workers=4,
     persistent_workers=True,
     sampler=dict(type=DefaultSampler, shuffle=True),
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
         data_mode=data_mode,
-        ann_file='annotations/person_keypoints_train2017.json',
-        data_prefix=dict(img='train2017/'),
+        ann_file='train.json',
+        data_prefix=dict(img='image'),
         pipeline=train_pipeline,
     ))
 val_dataloader = dict(
-    batch_size=32,
-    num_workers=2,
+    batch_size=4,
+    num_workers=4,
     persistent_workers=True,
     drop_last=False,
     sampler=dict(type=DefaultSampler, shuffle=False, round_up=False),
@@ -153,10 +136,8 @@ val_dataloader = dict(
         type=dataset_type,
         data_root=data_root,
         data_mode=data_mode,
-        ann_file='annotations/person_keypoints_val2017.json',
-        bbox_file='data/coco/person_detection_results/'
-        'COCO_val2017_detections_AP_H_56_person.json',
-        data_prefix=dict(img='val2017/'),
+        ann_file='validation.json',
+        data_prefix=dict(img='image'),
         test_mode=True,
         pipeline=val_pipeline,
     ))
@@ -164,6 +145,6 @@ test_dataloader = val_dataloader
 
 # evaluators
 val_evaluator = dict(
-    type=CocoMetric,
+    type=PCKAccuracy,
     ann_file=data_root + 'annotations/person_keypoints_val2017.json')
 test_evaluator = val_evaluator
